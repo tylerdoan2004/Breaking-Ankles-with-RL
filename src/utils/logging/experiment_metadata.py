9147a26abcc3cbab44b5cdbb8faedb6babfb26b6
Aston Ki Chan <kic2@uci.edu>
2026-02-24 18:58:12 -0800

    Modified config_paths attribute of ExperimentMetadata to be of type ConfigPaths, a TypedDict. Implemented to_dict and to_yaml in ExperimentMetadata in src/utils/logging/experiment_metadata.py.

--- src/utils/logging/experiment_metadata.py
+++ src/utils/logging/experiment_metadata.py
@@ -1,9 +1,20 @@
 """
 This module provides the ExperimentMetadata class for representing metadata about an experiment.
 """
-from dataclasses import dataclass
+import yaml
+from dataclasses import asdict, dataclass
 from datetime import datetime
-from pathlib import Path
+from typing import Any, TypedDict
+
+
+ConfigPaths = TypedDict(
+    "ConfigPaths",
+    {
+        "training": str,
+        "evaluation/in_distribution": str,
+        "evaluation/out_of_distribution": str
+    }
+)
 
 
 @dataclass(frozen = True, kw_only = True)
@@ -15,5 +26,25 @@ class ExperimentMetadata:
     model_name: str
     timestamp: datetime
     seed: int
-    model_hyperparameters: dict
-    config_paths: dict[str, str]
+    model_hyperparameters: dict[str, Any]
+    config_paths: ConfigPaths
+
+    def to_dict(self) -> dict[str, Any]:
+        """
+        Converts the ExperimentMetadata object to a YAML-serializable dictionary.
+        
+        :return: A YAML-serializable dictionary representing the ExperimentMetadata object.
+        """
+        data = asdict(self)
+        data["timestamp"] = data["timestamp"].isoformat()
+        return data
+
+    def to_yaml(self, file_path: str) -> None:
+        """
+        Writes the ExperimentMetadata object to a YAML file.
+
+        :param file_path: The path to the YAML file to write.
+        :return: None.
+        """
+        with open(file_path, "w", encoding = "utf-8") as file:
+            yaml.safe_dump(self.to_dict(), file, sort_keys = False, allow_unicode = True)
