b4652ebeed95eca4d041339fe7d4661e4b95c5ae
Aston Ki Chan <kic2@uci.edu>
2026-02-24 18:03:47 -0800

    Implemented the ExperimentMetadata class in src/utils/logging/experiment_metadata.py.

---
+++ src/utils/logging/experiment_metadata.py
@@ -0,0 +1,19 @@
+"""
+This module provides the ExperimentMetadata class for representing metadata about an experiment.
+"""
+from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+
+
+@dataclass(frozen = True)
+class ExperimentMetadata:
+    """
+    A class for representing metadata about an experiment.
+    """
+    experiment_name: str
+    model_name: str
+    timestamp: datetime
+    seed: int
+    model_hyperparameters: dict
+    config_paths: dict[str, Path]
